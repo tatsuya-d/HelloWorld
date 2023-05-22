cef22ba876cdf5bf05afdff9d13e239f681f459a
tatsuya <daidohtatsuya@Mac.local>
2023-05-22 21:29:00 +0900

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+public class HelloWorld {
+    public static void main(String[]args){
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("第３回課題");
+        list.add("すごく");
+        list.add("苦戦した。");
+        for (int i = 0; i < list.size(); i++){
+            System.out.println(list.get(i));
+        }
+
+
+
+
+
+}
+}
