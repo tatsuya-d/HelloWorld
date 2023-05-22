cce31082bb064f1edc36fa4794a46fe6f6f5049f
tatsuya <daidohtatsuya@Mac.local>
2023-05-23 00:04:51 +0900

    提出

---
+++ src/Exception.java
@@ -0,0 +1,15 @@
+import java.io.*;
+public class Exception  {
+    public static void main(String[]args){
+        WriteFile wf = new WriteFile();
+        try {
+            wf.open();
+            wf.write();
+        }catch (IOException e){
+            System.out.println("IOExceptionが発生しました");
+            e.printStackTrace();
+        } finally {
+            wf.close();
+        }
+    }
+}
---
+++ src/WriteFile.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class WriteFile {
+    public void open(){
+        System.out.println("ファイルを開く");
+    }
+    public void write()throws IOException{
+        throw new IOException();
+    }
+    public void close(){
+        System.out.println("ファイルを閉じます");
+    }
+
+}
