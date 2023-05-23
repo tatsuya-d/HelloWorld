94c7fa366c7fd7870d3a99e206363cbddec5c337
tatsuya <daidohtatsuya@Mac.local>
2023-05-23 10:48:58 +0900

    修正

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,12 +1,14 @@
 import java.util.ArrayList;
 public class HelloWorld {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         ArrayList<String> list = new ArrayList<String>();
         list.add("第３回課題");
         list.add("すごく");
         list.add("苦戦した。");
-        for (int i = 0; i < list.size(); i++){
+        for (int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i));
+        }
+        }
         }
 
 
@@ -14,5 +16,5 @@ public class HelloWorld {
 
 
 
-}
-}
+
+
